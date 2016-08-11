5aae2a8fd8663ed37fb99fb7c751ae896e88426c
Sergio Gómez <sgomez@aspgems.com>
2016-08-11 11:03:26 +0200

    Updates

---
+++ app.1ca211df424f85317e93.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,3],[function(n,e,o){var t=o(1);document.body.appendChild(t()),o(2)},function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="Hello world",n}}]);
+//# sourceMappingURL=app.1ca211df424f85317e93.js.map
---
+++ style.ce7c22e0c231812e2045.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],{0:function(n,o,t){t(32),n.exports=t(36)},32:function(n,o){},36:function(n,o){n.exports={redButton:"_2huKopl75N60gphOF_vFlU"}}});
+//# sourceMappingURL=style.ce7c22e0c231812e2045.js.map
